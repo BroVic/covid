ecf9f04db38ce6ce2ac4efc37e27d87fd6449c03
Victor Ordu <victorordu@outlook.com>
2020-05-25 18:04:14 +0100

    Enable multi-country comparisons

--- shiny/server.R
+++ shiny/server.R
@@ -1,9 +1,29 @@
 library(shiny)
 source('globals.R')
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
   covid <- decide_and_execute_data_sourcing(dirs$.cache, prefix, today)
-  
+
+  observe({
+    numVars <- length(input$variable)
+    numCntry <- length(input$country)
+    if (numVars == 2 && numCntry > 1) {
+      
+      updateSelectInput(
+        session,
+        cntryInputId,
+        cntryInputLabel,
+        choices = get_country_names(covid),
+        selected = input$country[[1]]
+      )
+      
+      output$message <- renderText(paste("To display more than one country,",
+                                         "select only 1 of the variables."))
+    }
+    else if (numVars < 2)
+      output$message <- renderText("")
+  })
+
   output$myplot <-
-    renderPlot(create_ggplot(covid, input$country))
+    renderPlot(create_ggplot(covid, input$country, input$variable))
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,13 +1,56 @@
 library(shiny)
 source('globals.R')
 
-data <- readCovidObj(dirs$.cache, prefix)$data
-countries <- unique(data$countriesAndTerritories)
-
-shinyUI(
-  fluidPage(
-    selectInput('country', "Country", countries, selected = 'Nigeria'),
-    
-    mainPanel(plotOutput("myplot"), width = 12)
+local({
+  obj <- readCovidObj(dirs$.cache, prefix)
+  countries <- get_country_names(obj)
+  startCntry <- "Nigeria"
+  
+  nn <- colnames(obj$data)
+  vars <- structure(as.list(nn), names = nn)
+  cases <- vars$cases
+  deaths <- vars$deaths
+  
+  # Dimensions
+  wd <- list(
+    full = 12L,
+    right = 4L,
+    mid = 4L,
+    left = 4L
   )
-)
+  
+  
+  shinyUI(fluidPage(
+    
+    mainPanel(plotOutput("myplot"), width = wd$full),
+    
+    fluidRow(
+      column(width = wd$left,
+             inputPanel(
+               selectInput(cntryInputId,
+                           cntryInputLabel,
+                           countries,
+                           startCntry,
+                           TRUE),
+               
+               em("Select one or more.")
+             )),
+      
+      column(width = wd$mid,
+             inputPanel(
+               checkboxGroupInput(
+                 varInputId,
+                 varInputLabel,
+                 c("Cases" = cases,
+                   "Deaths" = deaths),
+                 selected = c(cases, deaths),
+                 inline = TRUE
+               ),
+               em(textOutput("message"))
+             )),
+      
+      column(width = wd$right)
+    )
+    
+  ))
+})
